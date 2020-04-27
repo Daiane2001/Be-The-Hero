088e3fe0881adeaa9a75357dd047980d8dc35eb3
Daiane2001 <daianeoliveira2001@outlook.com>
2020-04-26 23:30:53 -0300

    Front-end da aplicação

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './global.css';
+
+/**import Logon from './pages/Logon'; /não coloquei o arquivo index pq sempre que importamos uma
+pasta ele sempre procura o arquivo index*/
+
+import Routes from './routes'
+function App() {
+  return (
+    <Routes/>
+  );
+}
+
+export default App;
+ 
